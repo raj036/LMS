9095a011752f254e9d8d90679cf8d5ae7832bc15
raj036 <1998trivediraj@gmail.com>
2024-07-31 17:45:30 +0530

    get attendance api code

--- src/pages/Attendance/index.tsx
+++ src/pages/Attendance/index.tsx
@@ -1,11 +1,101 @@
+import React, { useState, useEffect } from "react";
 import Topbar from "components/Topbar";
-import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import axios from "helper/axios";
+import { useAuthContext } from "hooks/useAuthContext";
 
 const MyAttendance = () => {
+  
+  const { user }:any = useAuthContext();
+  const [studentData, setStudentData] = useState(null);
+  const [studentId, setStudentId] = useState(null);
+  const [events, setEvents] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [attendanceModalIsOpen, setAttendanceModalIsOpen] = useState(false);
+  const [newEvent, setNewEvent] = useState({
+    title: "",
+    date: "",
+    attendees: [],
+  });
+  const [currentEvent, setCurrentEvent] = useState(null);
+
+
+  useEffect(() => {
+    if (user) {
+      fetchStudentData();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (studentId) {
+      fetchAttendanceForStudent();
+    }
+  }, [studentId]);
+
+  const fetchStudentData = async () => {
+    try {
+      const response = await axios.get(`api/admission/${user.user_id}`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      setStudentData(response.data);
+      
+      setStudentId(response.data.student_id);
+      console.log(response.data.student_id)
+    } catch (error) {
+      console.error("Error fetching student data:", error);
+    }
+  };
+
+  const fetchAttendanceForStudent = async () => {
+    try {
+      const response = await axios.get(`/api/attendance/?student_ids=${studentId}`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      const attendanceEvents = response.data.map((attendance) => ({
+        title: `${attendance.status.join(", ")}`,
+        date: attendance.date,
+        backgroundColor: attendance.status.includes("present") ? "green" : "red",
+        borderColor: attendance.status.includes("present") ? "green" : "red",
+      }));
+      setEvents(attendanceEvents);
+    } catch (error) {
+      console.error("Error fetching attendance:", error);
+    }
+  };
+
+  const handleDateClick = (arg) => {
+    setNewEvent({ ...newEvent, date: arg.dateStr });
+    setModalIsOpen(true);
+  };
+
+  const handleEventClick = (info) => {
+    const event = events.find(
+      (event) =>
+        event.title === info.event.title && event.date === info.event.startStr
+    );
+    setCurrentEvent(event);
+    setAttendanceModalIsOpen(true);
+  };
+
   return (
     <>
       <Topbar heading={"Attendance"} />
-      <div className="container py-5">Coming Soon...</div>
+      <div className="w-[80%] mx-auto">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          weekends={true}
+          events={events}
+          dateClick={handleDateClick}
+          eventClick={handleEventClick}
+        />
+      </div>
     </>
   );
 };
